89056ae5cf3c222f0c3dbd570db3a6f3309c9e5b
Amir Saeid <amir@glgdgt.com>
2021-02-28 16:03:25 +0000

    Cover cases where no config file exists

--- src/test/scala/io/laserdisc/zio/oci/objectstorage/ObjectStorageSettingsTest.scala
+++ src/test/scala/io/laserdisc/zio/oci/objectstorage/ObjectStorageSettingsTest.scala
@@ -3,7 +3,7 @@ package io.laserdisc.zio.oci.objectstorage
 import com.oracle.bmc.Region
 import zio.test.DefaultRunnableSpec
 import zio.test.{assert, assertM}
-import zio.test.Assertion.{equalTo, startsWithString}
+import zio.test.Assertion.{equalTo, isNonEmptyString}
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider
 import com.oracle.bmc.auth.CustomerAuthenticationDetailsProvider
 import java.util.UUID
@@ -17,10 +17,10 @@ object ObjectStorageSettingsTest extends DefaultRunnableSpec {
             equalTo(ObjectStorageAuth(SimpleAuthenticationDetailsProvider.builder().build()).auth.getKeyId())
           )
         },
-        testM("no profile in config file") {
+        testM("no config file or invalid profile") {
           for {
             f <- ObjectStorageAuth.fromConfigFileProfile(UUID.randomUUID().toString()).flip.map(_.message)
-          } yield assert(f)(startsWithString("No profile named"))
+          } yield assert(f)(isNonEmptyString)
         }
       )
     )
