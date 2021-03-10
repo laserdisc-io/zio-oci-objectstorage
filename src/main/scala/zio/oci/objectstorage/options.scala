7ee82bbc4f03bd759cf0dad2c1769e0e45930b2c
Amir Saeid <amir@glgdgt.com>
2021-03-10 15:24:15 +0000

    Add limit to ListObjectsOptions

--- src/main/scala/zio/oci/objectstorage/options.scala
+++ src/main/scala/zio/oci/objectstorage/options.scala
@@ -1,7 +1,11 @@
 package zio.oci.objectstorage
 
-final case class ListObjectsOptions(prefix: Option[String])
+final case class ListObjectsOptions(prefix: Option[String], limit: Int)
 
 object ListObjectsOptions {
-  val default: ListObjectsOptions = ListObjectsOptions(None)
+  val default: ListObjectsOptions = ListObjectsOptions(None, Limit.Max)
+}
+
+object Limit {
+  val Max: Int = 1000
 }
